df97edf06d534de3244481392a670d9d5d933aa6
shwezka <ditto.conic-0g@icloud.com>
2025-01-19 18:45:45 +0300

    Изменения в Main.java

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
+
+import java.lang.reflect.Array;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Oh hello there");
-        System.out.println("General Kenobi!");
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        System.out.println("You said: " + s);
+        System.out.println("GG");
     }
 }
